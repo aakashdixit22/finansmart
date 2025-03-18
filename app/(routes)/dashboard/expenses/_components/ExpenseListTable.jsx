04036b341d5f531d7a78dad0e9915c82ef9cb0b3
aakashdixit22 <aakashdixit227@gmail.com>
2025-03-18 23:42:58 +0530

    5th

--- app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
+++ app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
@@ -17,32 +17,37 @@ function ExpenseListTable({ expensesList, refreshData }) {
       refreshData();
     }
   };
+
   return (
     <div className="mt-3">
       <h2 className="font-bold text-lg">Latest Expenses</h2>
-      <div className="grid grid-cols-4 rounded-tl-xl rounded-tr-xl bg-slate-200 p-2 mt-3">
+      <div className="hidden md:grid grid-cols-4 rounded-tl-xl rounded-tr-xl bg-slate-200 p-2 mt-3">
         <h2 className="font-bold">Name</h2>
         <h2 className="font-bold">Amount</h2>
         <h2 className="font-bold">Date</h2>
         <h2 className="font-bold">Action</h2>
       </div>
       {expensesList.map((expenses, index) => (
-        <div className="grid grid-cols-4 bg-slate-50 rounded-bl-xl rounded-br-xl p-2">
-          <h2>{expenses.name}</h2>
-          <h2>{expenses.amount}</h2>
-          <h2>{expenses.createdAt}</h2>
+        <div
+          key={index}
+          className="grid grid-cols-1 md:grid-cols-4 gap-2 bg-slate-50 rounded-xl p-2 mt-1 text-sm md:text-base"
+        >
+          <div className="md:hidden font-bold text-gray-500">Name:</div>
+          <h2 className="font-semibold md:font-normal">{expenses.name}</h2>
+
+          <div className="md:hidden font-bold text-gray-500">Amount:</div>
+          <h2 className="text-green-500">${expenses.amount}</h2>
+
+          <div className="md:hidden font-bold text-gray-500">Date:</div>
+          <h2 className="text-gray-500">{expenses.createdAt}</h2>
+
+          <div className="md:hidden font-bold text-gray-500">Action:</div>
           <h2
             onClick={() => deleteExpense(expenses)}
-            className="text-red-500 cursor-pointer"
+            className="text-red-500 cursor-pointer hover:underline"
           >
             Delete
           </h2>
-          {/* <h2>
-            <Trash
-              className="text-red-500 cursor-pointer"
-              onClick={() => deleteExpense(expenses)}
-            />
-          </h2> */}
         </div>
       ))}
     </div>
